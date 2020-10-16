2a9121301c0c0dfc63107c8b9e839aaea05377b6
travis0125 <travis0125@hotmail.com>
2020-10-17 05:35:31 +0800

    update-5

--- config/ecpay.php
+++ config/ecpay.php
@@ -2,7 +2,7 @@
 
 return [
     'ServiceURL' => env('ECPAY_SERVICE_URL', ''),
-	'MerchantID' => env('ECPAY_MERCHANT_ID', ''),
+    'MerchantID' => env('ECPAY_MERCHANT_ID', ''),
     'HashKey' => env('ECPAY_HASH_KEY', ''),
     'HashIV' => env('ECPAY_HASH_IV', ''),
     'EncryptType' => env('ECPAY_ENCRYPT_TYPE', ''),
